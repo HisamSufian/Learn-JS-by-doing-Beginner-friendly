8dfb3cce39ba632e7983d054ffa2cd045c04c0cc
Hisam Sufian <86905544+HisamSufian@users.noreply.github.com>
2025-11-04 18:49:16 +0000

    Add Problem 1 – Hello World

---
+++ 01_Basics/01_hello_world.js
@@ -0,0 +1,11 @@
+/**
+ * 🧩 Problem 1 – Hello World
+ * Print “Hello, JavaScript!” to the console.
+ */
+
+console.log("Hello, JavaScript!");
+
+/**
+ * ✅ Expected Output:
+ * Hello, JavaScript!
+ */
